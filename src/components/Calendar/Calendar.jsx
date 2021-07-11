e7f0a93edb2acef0d6525b92864e42860e9769fd
Кирилл Павловский <kirill-pavlovskij@rambler.ru>
2021-07-11 20:25:13 +0300

    added boilerplate for calendar

---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './calendar.module.css';
+
+const Calendar = () => {
+    return (
+        <div>
+            Calendar component
+        </div>
+    );
+};
+
+export default Calendar;

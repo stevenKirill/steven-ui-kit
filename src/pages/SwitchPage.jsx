c03a4aa75d9fccc1ceb4bad92435d3316b05d9f4
Кирилл Павловский <kirill-pavlovskij@rambler.ru>
2021-09-10 20:24:40 +0300

    added switch theme

--- src/pages/SwitchPage.jsx
+++ src/pages/SwitchPage.jsx
@@ -5,14 +5,33 @@ import Switch from '../components/Switch/Switch';
 
 const SwitchPage = () => {
   const [switched, setSwitched] = useState(false);
+  const [theme,setTheme] = useState(false);
+  const light = '#F7F6F2';
+  const dark = '#11324D';
     return (
         <Container>
           <H3 text="Компонент Switch"/>
-          <Switch
-            isToggled={switched}
-            onToggle={setSwitched}
-            isRounded={true}
-          />
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+            <div>
+              <Switch
+                isToggled={switched}
+                onToggle={setSwitched}
+                isRounded={true}
+              />
+            </div>
+            <div style={{
+              backgroundColor: `${theme ? light : dark}`,
+              width: '400px',
+              height: '200px',
+              padding: '10px'
+              }}>
+              <Switch
+                isToggled={theme}
+                onToggle={setTheme}
+                isRounded={false}
+              />
+            </div>
+          </div>
         </Container>
     );
 };

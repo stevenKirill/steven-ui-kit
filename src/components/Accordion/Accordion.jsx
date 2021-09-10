d51bee4ea807d60851e29c2a7dc11793d20aaf4e
Кирилл Павловский <kirill-pavlovskij@rambler.ru>
2021-09-10 20:13:33 +0300

    added accordion component

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -1,12 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faMinus, faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import classes from './accordion.module.css';
+
+// цвета
+const COLORS_CONSTANTS = {
+    BLUE: '#236bdf',
+    RED: '#e45327',
+    YELLOW: '#c8e42c',
+    GREEN: '#76DC8C',
+};
+
+// иконки
+const ICONS_CONSTANTS = {
+    PLUS: 'PLUS',
+    ARROWS: 'ARROWS'
+};
 
 /** Компоннет аккордион */
-const Accordion = () => {
-    return (
-        <div>
+const Accordion = ({
+    data,
+    openedDefaultIndex = 1,
+    color = 'BLUE',
+    icon = 'PLUS'
+}) => {
+
+    const [selected,setSelected] = useState(openedDefaultIndex - 1);
+
+    const selectItem = (i) => {
+        if (selected === i) {
+            return setSelected(null)
+        } 
+        setSelected(i)
+    };
+
+    let component = null;
+
+    if (data.length === 0) {
+        throw new Error('передайте массив')
+    };
+
+    const renderIcons = (i) => {
+        let iconComponents = null;
+        if (icon === ICONS_CONSTANTS.PLUS) {
             
-        </div>
-    );
+            iconComponents = (
+                <FontAwesomeIcon
+                    icon={selected === i ? faMinus : faPlus}
+                    size="s"
+                    color={'#c8d4e7'}
+                    className={classes.pointer}
+                />
+            )
+        } else if (icon === ICONS_CONSTANTS.ARROWS) {
+            iconComponents = (
+                <FontAwesomeIcon
+                    icon={selected === i ? faArrowDown : faArrowUp}
+                    size="s"
+                    color={'#c8d4e7'}
+                    className={classes.pointer}
+                />
+            )
+        };
+        return iconComponents;
+    };
+
+    if (data.length > 0) {
+        component = data.map((item,id) => {
+            const { title, description } = item;
+            const detectClass = selected === id ? `${classes.description} ${classes.show}` : `${classes.description}`;
+            return (
+                <div 
+                    key={`${title}_${description[0]}_${id}`} 
+                    className={classes.item}
+                    style={{backgroundColor: COLORS_CONSTANTS[color]}}
+                    onClick={() => selectItem(id)}
+                >
+                    <div className={classes.title}>
+                        <span>{title}</span>
+                        {renderIcons(id)}
+                    </div>
+                    <div 
+                        className={detectClass}
+                    >
+                            {description}
+                    </div>
+                </div>
+            )
+        });
+    };
+
+    return component;
 };
 
 export default Accordion
--- src/pages/AccordionPage.jsx
+++ src/pages/AccordionPage.jsx
@@ -3,11 +3,45 @@ import Accordion from '../components/Accordion/Accordion';
 import Container from '../components/Container/Container';
 import H3 from '../components/H3/H3';
 
+const DATA_FOR_ACCORDION = [
+  {
+    title: 'First block',
+    description: 'Think how hard physics would be if particles could think. All serious daring starts from within. Think how hard physics would be if particles could think. All serious daring starts from within.'
+  },
+  {
+    title: 'Second block',
+    description: "Love is the flower you've got to let grow. Without courage, wisdom bears no fruit. You give before you get Love is the flower you've got to let grow. Without courage, wisdom bears no fruit. You give before you get"
+  },
+  {
+    title: 'Third block',
+    description: 'Real magic in relationships means an absence of judgement of others. I never think of the future. It comes soon enough.'
+  },
+  {
+    title: 'Fourth block',
+    description: 'Courage is going from failure to failure without losing enthusiasm. The two most powerful warriors are patience and time. The two most powerful warriors are patience and time. The two most powerful warriors are patience and time.'
+  },
+  {
+    title: 'Fifth block',
+    description: 'Life is movement-we breathe, we eat, we walk, we move! Life is movement-we breathe, we eat, we walk, we move! Life is movement-we breathe, we eat, we walk, we move! Life is movement-we breathe, we eat, we walk, we move! Life is movement-we breathe, we eat, we walk, we move! Life is movement-we breathe, we eat, we walk, we move!'
+  },
+];
+
 const AccordionPage = () => {
     return (
         <Container>
           <H3 text="Компонент Accordion"/>
-          <Accordion/>
+          <Accordion 
+            data={DATA_FOR_ACCORDION}
+            openedDefaultIndex={1}
+            color="RED"
+            icon="PLUS"
+          />
+            <Accordion 
+            data={DATA_FOR_ACCORDION}
+            openedDefaultIndex={3}
+            color="GREEN"
+            icon="ARROWS"
+          />
         </Container>
     );
 };

845f1fc8c22ff332ffe54f2d1659b62b279bd75c
Кирилл Павловский <kirill-pavlovskij@rambler.ru>
2021-09-14 17:47:23 +0300

    added progress bar

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import classes from './progressbar.module.css';
 
-const ProgressBar = () => {
+const time = {
+    1: 'one-sec',
+    2: 'two-sec',
+    3: 'three-sec',
+    4: 'four-sec',
+    5: 'five-sec',
+}
+
+const ProgressBar = ({ mainTitle, config }) => {
+    if (config.length === 0) {
+        throw new Error("data shouldn't be empty");
+    }
     return (
-        <div>
-            Progress bar
+        <div className={classes.wrapper}>
+            <h4 style={{ textAlign: 'center' }}>{mainTitle}</h4>
+            {config.map((option,id) => {
+                const { title, percent, color, animationTime } = option;
+                return (
+                    <section key={`${option.title}_${id}`} className={classes.box}>
+                        <section className={classes.texts}>
+                            <div>{title}</div>
+                            <div>{percent}%</div>
+                        </section>
+                        <section className={classes.bar}>
+                            <div className={classes[time[animationTime]]} style={{
+                                backgroundColor: `${color}`,
+                                maxWidth: `${percent}%`,
+                                height: `20px`,
+                                }}
+                            />
+                        </section>
+                    </section>
+                )
+            })}
         </div>
     )
 }
--- src/pages/ProgressBarPage.jsx
+++ src/pages/ProgressBarPage.jsx
@@ -3,11 +3,44 @@ import Container from '../components/Container/Container';
 import H3 from '../components/H3/H3';
 import ProgressBar from '../components/ProgressBar/ProgressBar';
 
+const progressBarData = [
+  {
+    title: 'Квартира',
+    percent: 40,
+    color: '#236bdf',
+    animationTime: 1
+  },
+  {
+    title: 'Еда',
+    percent: 20,
+    color: '#e45327',
+    animationTime: 3
+  },
+  {
+    title: 'Одежда',
+    percent: 10,
+    color: '#c8e42c',
+    animationTime: 2.5
+  },
+  {
+    title: 'Сотовая связь',
+    percent: 5,
+    color: '#76DC8C',
+    animationTime: 5
+  },
+  {
+    title: 'Развлечения',
+    percent: 25,
+    color: '#b84de2',
+    animationTime: 2
+  }
+]
+
 const ProgressBarPage = () => {
     return (
         <Container>
           <H3 text="Компонент Progress Bar"/>
-          <ProgressBar/>
+          <ProgressBar mainTitle="Расходы" config={progressBarData}/>
         </Container>
     );
 };
